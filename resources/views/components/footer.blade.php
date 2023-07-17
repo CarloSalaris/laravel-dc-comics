7b6d9365d9e46047f5ca5ff9fa7db461f47686a5
CarloSalaris <carloa.salaris@gmail.com>
2023-07-17 16:53:06 +0200

    MainController + main-layout + comics.index

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark text-light text-center py-3 mt-4">
+    <h1>FOOTER</h1>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,3 @@
+<header class="bg-dark text-light text-center py-3 mb-4">
+    <H1>DC COMICS</H1>
+</header>
