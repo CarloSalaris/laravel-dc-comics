186b01a67a9002021b529164e0b462efb8bfdb37
CarloSalaris <carloa.salaris@gmail.com>
2023-07-17 16:56:06 +0200

    output list of comics titles

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Hello World!</h1>
+    <h1>List of comics</h1>
+
+    <div class="container">
+        <ul>
+            @foreach ($comics as $comic)
+                <li> {{ $comic->title }} </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
